6339c99e2535e01417bb1a9105bb7ff659735491
Alex <alexander7dk@gmail.com>
2023-02-19 21:58:01 -0800

    got rid of useless things

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 style={{ border: '1px solid red' }} className="text-yellow-600">
-        Alex's Gunn App
-      </h1>
+      <h1 className="text-yellow-600">Alex's Gunn App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
